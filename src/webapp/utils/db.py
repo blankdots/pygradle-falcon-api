ea8285afb88bee184ea2962a09b34be0d9073382
Stefan Negru <stefan.negru@helsinki.fi>
2017-04-28 15:24:03 +0300

    adding files

---
+++ src/webapp/utils/db.py
@@ -0,0 +1,39 @@
+import sqlite3
+from webapp.utils.logs import app_logger
+
+
+def connect_DB(db_file=None):
+    """Connect to DB by parsing configuration."""
+    db_filename = ''
+    if db_file is None:
+        db_filename = 'data/data.db'
+    else:
+        db_filename = db_file
+    try:
+        conn = sqlite3.connect(db_filename)
+        app_logger.info('Connecting to database.')
+        tb_indexes_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='indexes'"
+        if not conn.execute(tb_indexes_exists).fetchone():
+            create_table(conn, """create table if not exists indexes (indexstatus text, data text, author text, timestamp datetime)""")
+        return conn
+    except Exception as error:
+        app_logger.error('Connection Failed!\
+            \nError Code is {0};\
+            \nError Content is {1};'.format(error.args[0], error.args[1]))
+        return error
+
+
+def create_table(conn, create_table_sql):
+    """Create a table from the create_table_sql statement.
+
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        db_cursor = conn.cursor()
+        db_cursor.execute(create_table_sql)
+        app_logger.info('Creating tables in the database if they do not exist.')
+    except Exception as error:
+        app_logger.error('Error {0}'.format(error))
+        return error
---
+++ src/webapp/webapi.py
@@ -0,0 +1,79 @@
+import falcon
+import time
+import json
+from datetime import datetime
+from webapp.utils.logs import app_logger
+from webapp.schemas import load_schema
+from webapp.utils.validate import validate
+from webapp.applib.registerdata import IndexingData
+
+api_version = "0.1"
+
+
+class HealthCheck(object):
+    """Create HealthCheck class."""
+
+    def on_get(self, req, resp):
+        """Respond on GET request to map endpoint."""
+        resp.status = falcon.HTTP_200
+        app_logger.info('Finished operations on /health GET Request.')
+
+
+class IndexClass(object):
+    """Create Indexing class for API."""
+
+    @validate(load_schema('index'))
+    def on_post(self, req, resp, parsed):
+        """Respond on GET request to index endpoint."""
+        data = IndexingData()
+        timestamp = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        index_args = [parsed.get('data'), parsed.get('author'), timestamp]
+        response = data.create_index(*index_args)
+        resp.data = json.dumps(response, indent=1, sort_keys=True)
+        resp.content_type = 'application/json'
+        resp.status = falcon.HTTP_202
+        app_logger.info('Creating/POST new data with ID: {0}.'.format(response['id']))
+
+
+class IndexingResource(object):
+    """Create Indexing Resource based on ID for retrieval."""
+
+    @validate(load_schema('idtype'))
+    def on_get(self, req, resp, indexID):
+        """Respond on GET request to index endpoint."""
+        data = IndexingData()
+        response = data.retrieve_indexID(indexID)
+        if response is not None:
+            resp.data = json.dumps(response, indent=1, sort_keys=True)
+            resp.content_type = 'application/json'
+            resp.status = falcon.HTTP_200
+            app_logger.info('GET /index the data with ID: {0}.'.format(indexID))
+        else:
+            raise falcon.HTTPGone()
+            app_logger.warning('Indexing job with ID: {0} is gone/deleted.'.format(indexID))
+
+    def on_delete(self, req, resp, indexID):
+        """Respond on GET request to index endpoint."""
+        data = IndexingData()
+        data.delete_indexID(indexID)
+        resp.data = json.dumps({"deletedID": indexID}, indent=1, sort_keys=True)
+        resp.content_type = 'application/json'
+        app_logger.info('Deleted/DELETE /index data with ID: {0}.'.format(indexID))
+        resp.status = falcon.HTTP_200
+
+
+def createAPI():
+    """Create the API endpoint."""
+    do_index = IndexClass()
+    get_index = IndexingResource()
+
+    webapp = falcon.API()
+    webapp.add_route('/health', HealthCheck())
+    webapp.add_route('/%s/index' % (api_version), do_index)
+    webapp.add_route('/%s/index/{indexID}' % (api_version), get_index)
+    app_logger.info('App is running.')
+    return webapp
+
+
+if __name__ == '__main__':
+    createAPI()
